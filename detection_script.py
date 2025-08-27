e7c033ae443ae2c9257d208e58f07770bd29ac2e
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-08-27 12:36:09 +0100

    added detection

--- detection_script.py
+++ detection_script.py
@@ -2,6 +2,7 @@ import psutil
 import os
 
 keylogger_file = "Python/Projects/Keylogger-and-Detection-Script/keylogger.py"
+text_log = "Python/Projects/Keylogger-and-Detection-Script/key_presses.txt"
 
 def detect_script():
     for proc in psutil.process_iter(["pid", "name", "username"]):
@@ -11,3 +12,13 @@ def detect_script():
             print("[+] Script was Found.")
         else:
             print("[+] Script not found yet.")
+
+def detect_text_log():
+    file_exists = os.path.exists(text_log)
+
+    if file_exists:
+        print("[+] Log was found")
+        return True
+    else:
+        print("[+] Log was not found")
+        return False
