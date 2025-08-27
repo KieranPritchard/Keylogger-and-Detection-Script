991f3a3f80fdd84fc6fb43d988662aed59de719d
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-08-27 14:16:13 +0100

    fixed the detection script

--- detection_script.py
+++ detection_script.py
@@ -1,13 +1,11 @@
 import psutil
 import os
 
-keylogger_file = "Python/Projects/Keylogger-and-Detection-Script/keylogger.py"
-text_log = "Python/Projects/Keylogger-and-Detection-Script/key_presses.txt"
-
-def detect_script():
-    for proc in psutil.process_iter(["pid", "name", "username"]):
+def detect_script(keylogger_file):
+    for proc in psutil.process_iter(["pid", "name", "username", "cmdline"]):
         try:
-            if proc.info["name"] == keylogger_file:
+            cmdline = proc.info.get("cmdline")
+            if cmdline and keylogger_file in cmdline:
                 print("[+] Script was found.")
                 return True, proc
         except Exception as e:
@@ -16,7 +14,7 @@ def detect_script():
     print("[+] Script not found.")
     return False, None
 
-def detect_text_log():
+def detect_text_log(text_log):
     file_exists = os.path.exists(text_log)
 
     if file_exists:
@@ -34,12 +32,15 @@ def delete_log(text_log):
     os.remove(text_log)
 
 def main():
+    keylogger_file = "Python/Projects/Keylogger-and-Detection-Script/keylogger.py"
+    text_log = "Python/Projects/Keylogger-and-Detection-Script/key_presses.txt"
+
     print("=" * 60)
     print("Keylogger Detection Script")
     print("=" * 60)
 
-    script_detection_result, keylogger_process = detect_script()
-    log_detection_result = detect_text_log()
+    script_detection_result, keylogger_process = detect_script(keylogger_file)
+    log_detection_result = detect_text_log(text_log)
 
     if script_detection_result == True and log_detection_result == True:
         terminate_script(keylogger_process)
